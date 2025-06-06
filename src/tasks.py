839b3e65f7a397bd599203e7a091aed31c78e203
Romain Gayon <romaing@google.com>
2025-06-06 09:38:37 +0000

    lint

--- src/tasks.py
+++ src/tasks.py
@@ -62,7 +62,7 @@ def calculate_entropy(data: bytes) -> float:
     for x in range(256):
         p_x = float(data.count(x)) / len(data)
         if p_x > 0:
-            entropy += -p_x * math.log(p_x, 2)
+            entropy -= p_x * math.log(p_x, 2)
     return entropy
 
 
